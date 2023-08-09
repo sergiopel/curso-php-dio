251d4e002fdb1d43c8bc57d330fcae9484a8a610
Sergio Pelegrini <serpel@gmail.com>
2023-08-09 09:38:14 -0300

    continuação do exemplo arquivo constante.php

--- tipos-de-variaveis/constante.php
+++ tipos-de-variaveis/constante.php
@@ -1,1 +1,13 @@
 <?php
+// constantes são valores salvos em memória que não podem ser alterados
+// por convenção são definidas com letras maiúsculas
+// pela sintaxe, não pode ter o cifrão antes 
+define("VALOR_DIA", "80");
+// outro modo de definir uma constante:
+const GORJETAS = "40";
+
+echo VALOR_DIA + GORJETAS;
+
+//constantes não podem ser alteradas, gera um erro no editor e na compilação:
+const GORJETAS = "50";
+echo GORJETAS;
